7ca92501ce21da5f6efd1243854082ed21512824
xxcj <cyk19951112@gmail.com>
2025-04-28 23:59:51 +0800

    dataset_china

--- hydrodata_china/settings/datasets_dir.py
+++ hydrodata_china/settings/datasets_dir.py
@@ -4,7 +4,7 @@
 @Company:            Dalian University of Technology
 @Date:               2025-04-26 11:06:32
 @Last Modified by:   Yikai CHAI
-@Last Modified time: 2025-04-26 15:08:32
+@Last Modified time: 2025-04-28 23:56:42
 """
 
 
@@ -13,23 +13,23 @@ from hydrodatautils.foundation import hydro_dirction
 
 DATASETS_DIR = {
     "Sanxia_1D": {
-        "ROOT_DIR": Path(hydro_dirction.get_origin_dir(dataset_type="Dataset_China", dataset_name="Sanxia_Project")),
-        "CACHE_DIR": Path(hydro_dirction.get_cache_dir(dataset_type="Dataset_China", dataset_name="Sanxia_1D")),
-        "EXPORT_DIR": Path(hydro_dirction.get_export_dir(dataset_type="Dataset_China", dataset_name="Sanxia_1D")),
+        "ROOT_DIR": Path(hydro_dirction.get_origin_dir(dataset_type="Dataset_CHINA", dataset_name="Sanxia_Project")),
+        "CACHE_DIR": Path(hydro_dirction.get_cache_dir(dataset_type="Dataset_CHINA", dataset_name="Sanxia_1D")),
+        "EXPORT_DIR": Path(hydro_dirction.get_export_dir(dataset_type="Dataset_CHINA", dataset_name="Sanxia_1D")),
     },
     "Sanxia_1H": {
-        "ROOT_DIR": Path(hydro_dirction.get_origin_dir(dataset_type="Dataset_China", dataset_name="Sanxia_Project")),
-        "CACHE_DIR": Path(hydro_dirction.get_cache_dir(dataset_type="Dataset_China", dataset_name="Sanxia_1H")),
-        "EXPORT_DIR": Path(hydro_dirction.get_export_dir(dataset_type="Dataset_China", dataset_name="Sanxia_1H")),
+        "ROOT_DIR": Path(hydro_dirction.get_origin_dir(dataset_type="Dataset_CHINA", dataset_name="Sanxia_Project")),
+        "CACHE_DIR": Path(hydro_dirction.get_cache_dir(dataset_type="Dataset_CHINA", dataset_name="Sanxia_1H")),
+        "EXPORT_DIR": Path(hydro_dirction.get_export_dir(dataset_type="Dataset_CHINA", dataset_name="Sanxia_1H")),
     },
     "Anhui_1D": {
-        "ROOT_DIR": Path(hydro_dirction.get_origin_dir(dataset_type="Dataset_China", dataset_name="Anhui_Project")),
-        "CACHE_DIR": Path(hydro_dirction.get_cache_dir(dataset_type="Dataset_China", dataset_name="Anhui_1D")),
-        "EXPORT_DIR": Path(hydro_dirction.get_export_dir(dataset_type="Dataset_China", dataset_name="Anhui_1D")),
+        "ROOT_DIR": Path(hydro_dirction.get_origin_dir(dataset_type="Dataset_CHINA", dataset_name="Anhui_Project")),
+        "CACHE_DIR": Path(hydro_dirction.get_cache_dir(dataset_type="Dataset_CHINA", dataset_name="Anhui_1D")),
+        "EXPORT_DIR": Path(hydro_dirction.get_export_dir(dataset_type="Dataset_CHINA", dataset_name="Anhui_1D")),
     },
     "Anhui_1H": {
-        "ROOT_DIR": Path(hydro_dirction.get_origin_dir(dataset_type="Dataset_China", dataset_name="Anhui_Project")),
-        "CACHE_DIR": Path(hydro_dirction.get_cache_dir(dataset_type="Dataset_China", dataset_name="Anhui_1H")),
-        "EXPORT_DIR": Path(hydro_dirction.get_export_dir(dataset_type="Dataset_China", dataset_name="Anhui_1H")),
+        "ROOT_DIR": Path(hydro_dirction.get_origin_dir(dataset_type="Dataset_CHINA", dataset_name="Anhui_Project")),
+        "CACHE_DIR": Path(hydro_dirction.get_cache_dir(dataset_type="Dataset_CHINA", dataset_name="Anhui_1H")),
+        "EXPORT_DIR": Path(hydro_dirction.get_export_dir(dataset_type="Dataset_CHINA", dataset_name="Anhui_1H")),
     },
 }
