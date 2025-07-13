859c3ef550f3b2eb7670ee19b639dfe0fa816477
xxcj <cyk19951112@gmail.com>
2025-07-13 15:19:41 +0800

    dir

--- hydrodata_china/settings/datasets_dir.py
+++ hydrodata_china/settings/datasets_dir.py
@@ -12,16 +12,6 @@ from pathlib import Path
 from hydrodatautils.foundation import hydro_dirction
 
 DATASETS_DIR = {
-    "Sanxia_1D": {
-        "ROOT_DIR": Path(hydro_dirction.get_origin_dir(dataset_type="Dataset_CHINA", dataset_name="Sanxia_Project")),
-        "CACHE_DIR": Path(hydro_dirction.get_cache_dir(dataset_type="Dataset_CHINA", dataset_name="Sanxia_1D")),
-        "EXPORT_DIR": Path(hydro_dirction.get_export_dir(dataset_type="Dataset_CHINA", dataset_name="Sanxia_1D")),
-    },
-    "Sanxia_1H": {
-        "ROOT_DIR": Path(hydro_dirction.get_origin_dir(dataset_type="Dataset_CHINA", dataset_name="Sanxia_Project")),
-        "CACHE_DIR": Path(hydro_dirction.get_cache_dir(dataset_type="Dataset_CHINA", dataset_name="Sanxia_1H")),
-        "EXPORT_DIR": Path(hydro_dirction.get_export_dir(dataset_type="Dataset_CHINA", dataset_name="Sanxia_1H")),
-    },
     "Anhui_1H": {
         "ROOT_DIR": Path(hydro_dirction.get_origin_dir(dataset_type="Dataset_CHINA", dataset_name="Anhui_Project")),
         "CACHE_DIR": Path(hydro_dirction.get_cache_dir(dataset_type="Dataset_CHINA", dataset_name="Anhui_1H")),
